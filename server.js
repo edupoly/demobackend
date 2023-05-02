d23c32d656e004680879465ac58c1a928fc7335d
edupoly <info.edupoly@gmail.com>
2023-05-02 18:24:04 +0530

    sdfsdf

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-console.log("Hello world");
-console.log(process.env.PORT)
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+server.listen(process.env.PORT||4000, () => {
+  console.log('JSON Server is running')
+})
